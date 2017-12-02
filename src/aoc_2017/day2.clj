c2a3ce27c5cacdf7fb6400e41243efa4fd4230b0
Paul Whyler <pmwhyle@gmail.com>
2017-12-02 08:02:29 +0000

    day 2 done

--- src/aoc_2017/day2.clj
+++ src/aoc_2017/day2.clj
@@ -1,5 +1,6 @@
 (ns aoc-2017.day2
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.pprint :refer [pprint]]))
 
 (def input "resources/day2/input.txt")
 
@@ -8,9 +9,21 @@
        (map #(Integer/parseInt %))
        (#(- (apply max %) (apply min %)))))
 
-(defn checksum [s]
+(defn divisible [coll]
+  (let [coll (map #(Integer/parseInt %) coll)]
+    (loop [num (first coll)
+           numr (next coll)
+           den (first coll)
+           denr (next coll)]
+      (if (and (not= num den) (zero? (mod num den)))
+        (/ num den)
+        (if denr
+          (recur num numr (first denr) (next denr))
+          (when numr (recur (first numr)(next numr) (first coll) (next coll))))))))
+
+(defn checksum [f s]
   (->> s
        (#(str/split % #"\n"))
        (map #(str/split % #"\s+"))
-       (map diff)
+       (map f)
        (reduce + 0)))
--- test/aoc_2017/day2_test.clj
+++ test/aoc_2017/day2_test.clj
@@ -4,7 +4,8 @@
 
 (deftest day2
   (testing "part 1"
-    (is (= 18 (sut/checksum "5 1 9 5\n7 5 3\n2 4 6 8")))
-    (is (= 0 (sut/checksum (slurp sut/input)))))
+    (is (= 18 (sut/checksum sut/diff "5 1 9 5\n7 5 3\n2 4 6 8")))
+    (is (= 43074 (sut/checksum sut/diff (slurp sut/input)))))
   (testing "part 2"
-    (is (= true false))))
+    (is (= 9 (sut/checksum sut/divisible "5 9 2 8\n9 4 7 3\n3 8 6 5")))
+    (is (= 280 (sut/checksum sut/divisible (slurp sut/input))))))
