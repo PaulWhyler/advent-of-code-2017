1055aed13d18ab6323a3bde2f03f368aa8d86d76
Paul Whyler <pmwhyle@gmail.com>
2017-12-04 23:01:50 +0000

    Done day3 part 1

--- src/aoc_2017/day3.clj
+++ src/aoc_2017/day3.clj
@@ -2,11 +2,37 @@
   (:require [clojure.string :as str]
             [clojure.pprint :refer [pprint]]))
 
-(defn distance [start] 0)
+(defn find-last-in-circle [n]
+  (loop [r 3]
+    (if (>= (* r r) n)
+      r
+      (recur (+ 2 r)))))
+
+(defn find-corner [r n]
+  (let [segment-size (/ (dec r) 2)
+        last-corner (* (- r 2) (- r 2))]
+    (loop [corner (* r r)]
+      (if (= corner last-corner)
+        :invalid
+        (cond
+          (= (- corner segment-size) n)
+          segment-size
+          (<= (- corner segment-size) n corner)
+          (- (+ segment-size segment-size) (- corner n))
+          (< (- corner segment-size segment-size) n (- corner segment-size))
+          (- (* 2 segment-size) (- n (- corner segment-size segment-size)))
+          :else
+          (recur (- corner segment-size segment-size)))))))
+
+(defn distance [start]
+  (if (= 1 start)
+    0
+    (let [r (find-last-in-circle start)]
+      (find-corner r start))))
 
 
 
-(comment
+#_(comment
 
   number in each circle
   1
@@ -25,34 +51,36 @@
 72 43 44 45 46 47 48 49 50
 73 74 75 76 77 78 79 80 81 82
 
-last number in each circle:
-1 = 1^2
-9  = 3^2
-25 = 5^2
-49 = 7^2
-81 = 9^2
+last number in each circle
+1 = 1 ** 2
+9  = 3 ** 2
+25 = 5 ** 2
+49 = 7 ** 2
+81 = 9 ** 2
 
 e.g. 1024
-From the last number of the circle that contains the target:  1089 = 33^2
-Take 2 from the sqrt : 31. + 1 and half  = 16
-Find segment containing number:
-1089 - 16 = 1073 : no
-1073 - 16 = 1057 : no
-1057 - 16 = 1041 : no
-1041 - 16 = 1025 : no
-1025 - 16 = 1009 : yes
+From the last number of the circle that contains the target  1089 = 33 ** 2
+Take 2 from the sqrt  31. + 1 and half  = 16
+Find segment containing number
+1089 - 16 = 1073  no
+1073 - 16 = 1057  no
+1057 - 16 = 1041  no
+1041 - 16 = 1025  no
+1025 - 16 = 1009  yes
 
-difference between last number and target: 1025 - 1024 = 1
+difference between last number and target 1025 - 1024 = 1
 
-Manhatten distance =
+Manhatten distance = 33 - 1 - that difference = 31.
 
-first number on each circle:
+So, find the right root, find the segment, add or subtract
+
+first number on each circle
 1
 2
     10 = 8 + 2   = start loop 3
-+16 26 = 3*8 + 2 = start loop 4
-+24 50 = 6*8 + 2 = start loop 5
-+32 82 = 10*8 + 2 = start loop 6
++16 26 = 3 * 8 + 2 = start loop 4
++24 50 = 6 * 8 + 2 = start loop 5
++32 82 = 10 * 8 + 2 = start loop 6
 +40 122 = 15 * 8 + 2 = start loop 7
 +48 170 = 21 * 8 + 2 = start loop 8
 +56 226 = 28 * 8 + 2 = start loop 9
--- test/aoc_2017/day3_test.clj
+++ test/aoc_2017/day3_test.clj
@@ -3,10 +3,23 @@
             [clojure.test :refer [deftest is testing]]))
 
 (deftest day3
+  (testing "helpers"
+    (is (= 33 (sut/find-last-in-circle 1024)))
+    (is (= 7 (sut/find-last-in-circle 45))))
   (testing "part 1"
     (is (= 0 (sut/distance 1)))
     (is (= 3 (sut/distance 12)))
     (is (= 2 (sut/distance 23)))
-    (is (= 31 (sut/distance 1024))))
-  (testing "part 2"
+    (is (= 31 (sut/distance 1024)))
+    (is (= 4 (sut/distance 25)))
+    (is (= 4 (sut/distance 21)))
+    (is (= 4 (sut/distance 17)))
+    (is (= 4 (sut/distance 13)))
+    (is (= 2 (sut/distance 9)))
+    (is (= 6 (sut/distance 31)))
+    (is (= 4 (sut/distance 53)))
+    (is (= 5 (sut/distance 52)))
+    (is (= 6 (sut/distance 51)))
+    (is (= 430 (sut/distance 312051))))
+  #_(testing "part 2"
     (is (= true false))))
