7b02f9879cd466cf4cbd3f1dd6cd4204d8b299c0
Paul Whyler <pmwhyle@gmail.com>
2018-04-04 23:02:00 +0100

    Done part 1

--- src/aoc_2017/day7.clj
+++ src/aoc_2017/day7.clj
@@ -18,11 +18,14 @@
 (s/def ::children (s/+ ::name))
 (s/def ::node (s/cat :name ::name :weight ::weight :sep (s/? #{'->}) :children (s/? ::children)))
 
+(defn parse [s]
+  (->> s
+       (.split #"\n")
+       (map #(edn/read-string (str "[" % "]")))))
+
 (defn read-input [filename]
   (->> (str "resources/day7/" filename)
-       slurp
-       (#(str/split % #"\s+"))
-       (into [])))
+       slurp))
 
 
 (def a (slurp "resources/day7/input.txt"))
@@ -35,20 +38,29 @@
 
 (take 2 (s/exercise ::node))
 
-(defn add-line-to-graph [{:keys [nodes edges] :as graph} {:keys [name weight children]}]
+(defn add-line-to-graph [{:keys [nodes edges] :as graph} {:keys [name weight parents]}]
   (-> graph
       (update-in [:nodes] conj {:name name :weight weight})
-      (update-in [:edges] #(apply conj % (map (fn [child] #{name child}) children)))))
+      (update-in [:edges] #(if parents (assoc % name (set parents)) %)
+                 #_#(apply conj % (map (fn [child] #{name child}) children)))))
 
 (defn into-graph [lines]
   (reduce add-line-to-graph
-          {:nodes #{} :edges #{}}
+          {:nodes #{} :edges {}}
           lines))
 
-(defn all-into-graph [file]
-  (->> file
-       (slurp)
-       (.split #"\n")
-       (map #(edn/read-string (str "[" % "]")))
+(defn all-into-graph [s]
+  (->> s
+       parse
        (map (partial s/conform ::node))
        into-graph))
+
+(defn bottom [graph]
+  (loop [node (:name (first (:nodes graph)))
+         edges (rest (:edges graph))
+         edge (first (:edges graph))]
+   (if (not edge)
+      node
+      (if (contains? (second edge) node)
+        (recur (first edge) (rest (:edges graph)) (first (:edges graph)))
+        (recur node (rest edges) (first edges))))))
--- test/aoc_2017/day7_test.clj
+++ test/aoc_2017/day7_test.clj
@@ -1,5 +1,5 @@
 (ns aoc-2017.day7-test
-  (:require [aoc-2017.day7 :refer [read-input into-graph bottom add-line-to-graph]]
+  (:require [aoc-2017.day7 :refer [read-input into-graph bottom add-line-to-graph all-into-graph parse]]
             [clojure.test :refer [deftest is testing]]))
 
 (def sample1
@@ -17,12 +17,33 @@ ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)")
 
+(def s1-graph
+  {:nodes #{{:name "gyxo", :weight 61}
+            {:name "ugml", :weight 68}
+            {:name "padx", :weight 45}
+            {:name "ktlj", :weight 57}
+            {:name "pbga", :weight 66}
+            {:name "fwft", :weight 72}
+            {:name "havc", :weight 66}
+            {:name "xhth", :weight 57}
+            {:name "ebii", :weight 61}
+            {:name "qoyq", :weight 66}
+            {:name "tknk", :weight 41}
+            {:name "jptl", :weight 61}
+            {:name "cntj", :weight 57}},
+   :edges
+   {"ugml" #{"gyxo" "ebii" "jptl"}
+    "fwft" #{"cntj" "xhth" "ktlj"}
+    "padx" #{"pbga" "havc" "qoyq"}
+    "tknk" #{"ugml" "fwft" "padx"}}})
+
 (deftest day7
   (testing "part 1"
-    (is (= {:nodes #{{:name "a" :weight 10}} :edges #{#{"a" "b"}#{"a" "c"}}}
-           (add-line-to-graph {:nodes #{} :edges #{}} {:name "a" :weight 10 :children ["b" "c"]})))
-    (is (= "tknk" (bottom sample1)))
-    (is (= "?" (bottom (read-input "input.txt")))))
+    (is (= {:nodes #{{:name "a" :weight 10}} :edges {"a" #{"b" "c"}}}
+           (add-line-to-graph {:nodes #{} :edges {}} {:name "a" :weight 10 :parents ["b" "c"]})))
+    (is (= s1-graph (all-into-graph sample1)))
+    (is (= "tknk" (bottom (all-into-graph sample1))))
+    (is (= "eqgvf" (bottom (all-into-graph  (read-input "input.txt"))))))
   (testing "part 2"
 
     ))
